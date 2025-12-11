cce18dbdb32e880a64a9a5228494c68641cbb1cf
James Waller <James@chosendaily.com>
2025-12-10 16:23:17 -0800

    Add files via upload

--- components/ui/accordion.tsx
+++ components/ui/accordion.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
 import { ChevronDown } from "lucide-react"
@@ -25,7 +27,7 @@ const AccordionTrigger = React.forwardRef
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:no-underline [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
@@ -49,7 +51,6 @@ const AccordionContent = React.forwardRef
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ))
-
 AccordionContent.displayName = AccordionPrimitive.Content.displayName
 
 export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
